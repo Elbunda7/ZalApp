4ecb8f0aa997cae74b9a7abfafe5c48fd5e7b674
Michal Břemek <michal.bremek@seznam.cz>
2018-12-29 12:22:45 +0100

    Přidejte soubory projektu.

---
+++ ZalApp/ZalApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ZalApp.Models;
+
+namespace ZalApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
